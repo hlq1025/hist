53443d8770f8e23871a114161ce8c30e95b424ee
hlq <1606281400@qq.com>
2018-10-16 10:43:56 +0800

    second commit

--- 颜色直方图--opencl/main.cpp
+++ 颜色直方图--opencl/main.cpp
@@ -1,142 +1,334 @@
-// main.c
-#include <CL/cl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <time.h>
+#include <stdlib.h>  
 
-#define SIZE_OF_BIN 256
+#include <string.h>  
 
-char *sourceText = "kernel.cl";
+#include <iostream>  
 
-int readText(const char* kernelPath, char **pcode)// ËØªÂèñÊñáÊú¨Êñá‰ª∂ÊîæÂÖ• pcodeÔºåËøîÂõûÂ≠óÁ¨¶‰∏≤ÈïøÂ∫¶
+/* OpenCL includes */
+
+#include <CL/cl.h>  
+
+#include <opencv2/opencv.hpp>
+
+#include <opencv2/highgui.hpp>
+
+#include <opencv2/imgproc.hpp>
+
+#include<fstream>
+
+
+using namespace cv;
+
+using namespace std;
+
+static const int HIST_BINS = 256;
+cl_int ConvertToString(const char *pFileName, std::string &Str)
+{	size_t		uiSize = 0;
+	size_t		uiFileSize = 0;
+	char		*pStr = NULL;
+	std::fstream fFile(pFileName, (std::fstream::in | std::fstream::binary));
+	if (fFile.is_open())
+{		fFile.seekg(0, std::fstream::end);
+		uiSize = uiFileSize = (size_t)fFile.tellg();  // ªÒµ√Œƒº˛¥Û–°
+		fFile.seekg(0, std::fstream::beg);
+		pStr = new char[uiSize + 1];
+		if (NULL == pStr)
 {
-	FILE *fp;
-	int size;
-	//printf("<readText> File: %s\n", kernelPath);
-	fopen_s(&fp, kernelPath, "rb");
-	if (!fp)
-	{
-		printf("<readText> Open file failed\n");
-		getchar();
-		exit(-1);
-	}
-	if (fseek(fp, 0, SEEK_END) != 0)
-	{
-		printf("<readText> Seek end of file failed\n");
-		getchar();
-		exit(-1);
-	}
-	if ((size = ftell(fp)) < 0)
-	{
-		printf("<readText> Get file position failed\n");
-		getchar();
-		exit(-1);
-	}
-	rewind(fp);
-	if ((*pcode = (char *)malloc(size + 1)) == NULL)
-	{
-		printf("<readText> Allocate space failed\n");
-		getchar();
-		exit(-1);
-	}
-	fread(*pcode, 1, size, fp);
-	(*pcode)[size] = '\0';
-	fclose(fp);
-	return size + 1;
+			fFile.close();
+			return 0;
 }
 
-int main(int argc, char * argv[])
+
+
+
+
+
+
+		fFile.read(pStr, uiFileSize);				// ∂¡»°uiFileSize◊÷Ω⁄
+
+
+
+		fFile.close();
+
+
+
+		pStr[uiSize] = '\0';
+
+
+
+		Str = pStr;
+
+
+
+
+
+
+
+		delete[] pStr;
+
+
+
+
+
+
+
+		return 0;
+
+
+
+	}
+
+
+
+
+
+
+
+	cout << "Error: Failed to open cl file\n:" << pFileName << endl;
+
+
+
+
+
+
+
+	return -1;
+
+
+
+}
+
+void check(cl_int status){
+
+	if (status != CL_SUCCESS)
+
+	{
+
+		cout << "error:status is " << status << endl;
+
+		return;
+
+	}
+
+}
+
+
+int main()
 {
-	//_putenv("GPU_DUMP_DEVICE_KERNEL=3");// Âú®Á®ãÂ∫èÁõÆÂΩïËæìÂá∫Âá∫ il Âíå isa ÂΩ¢ÂºèÁöÑ kernel Êñá‰ª∂ÔºåÂèØ‰ª•‰ΩøÁî® isa Ê±áÁºñË∞ÉËØï
+	Mat img = imread("1.jpg");
+
+	int imageRows = img.rows;
+
+	int imageCols = img.cols;
+
+	//CPU result,reconfirm the GPU result...
+
+	int rgbhistogram[256 * 3];
+
+	memset(rgbhistogram, 0, 256 * 3 * sizeof(int));
+
+	for (int i = 0; i<imageRows; i++)
+
+	{
+
+		uchar *rowdatas = img.ptr<uchar>(i);
+
+		for (int j = 0; j<imageCols * 3; j += 3)
+
+		{
+
+			int r = rowdatas[j + 2];
+
+			int g = rowdatas[j + 1];
+
+			int b = rowdatas[j];
+
+			rgbhistogram[r * 3] += 1;
+
+			rgbhistogram[g * 3 + 1] += 1;
+
+			rgbhistogram[b * 3 + 2] += 1;
+
+		}
+
+	}
+
+	const int imageElements = imageRows*imageCols;
+
+	const size_t imageSize = imageElements*sizeof(unsigned char)* 3;
+
+	const int histogramSize = HIST_BINS*sizeof(int);
+
+	int *hOutputHistogramR = (int*)malloc(histogramSize);
+
+	int *hOutputHistogramG = (int*)malloc(histogramSize);
+
+	int *hOutputHistogramB = (int*)malloc(histogramSize);
 
 	cl_int status;
+	
 	cl_uint nPlatform;
+	
 	clGetPlatformIDs(0, NULL, &nPlatform);
+	
 	cl_platform_id *listPlatform = (cl_platform_id*)malloc(nPlatform * sizeof(cl_platform_id));
+	
 	clGetPlatformIDs(nPlatform, listPlatform, NULL);
+
 	cl_uint nDevice = 0;
-	clGetDeviceIDs(listPlatform[0], CL_DEVICE_TYPE_ALL, 0, NULL, &nDevice);
-	cl_device_id *listDevice = (cl_device_id*)malloc(nDevice * sizeof(cl_device_id));
-	clGetDeviceIDs(listPlatform[0], CL_DEVICE_TYPE_ALL, nDevice, listDevice, NULL);
-	cl_context context = clCreateContext(NULL, nDevice, listDevice, NULL, NULL, &status);
-	cl_command_queue queue = clCreateCommandQueue(context, listDevice[0], 0, &status);
 
-	char *code;
-	size_t length = readText(sourceText, &code);
-	cl_program program = clCreateProgramWithSource(context, 1, (const char **)&code, &length, NULL);
-	status = clBuildProgram(program, 1, listDevice, NULL, NULL, NULL);
-	cl_kernel histogram = clCreateKernel(program, "histogramKernel", &status);
-	cl_kernel reduce = clCreateKernel(program, "reduceKernel", &status);
+	clGetDeviceIDs(listPlatform[0], CL_DEVICE_TYPE_GPU, 0, NULL, &nDevice);
+	
+	cl_device_id *device = (cl_device_id*)malloc(nDevice * sizeof(cl_device_id));
 
-	cl_uint nThreads = 1024;                                        // 64 * 1024
-	cl_uint nThreadsPerGroup = 256;                                 // ÂéüÂßãÂÄºÔºöKernelCompileWorkGroupSize[0]
-	cl_uint nGroups = nThreads / nThreadsPerGroup;
+	clGetDeviceIDs(listPlatform[0], CL_DEVICE_TYPE_GPU, nDevice, device, NULL);
 
-	cl_uint inputByte = 2147483648;                                 // ÂéüÂßãÂÄºÔºöDeviceMaxMemAllocSize == 2147483648 == 2^31
-	cl_uint outputNBytes = nGroups * SIZE_OF_BIN * sizeof(cl_uint);
-	cl_uint element = inputByte / sizeof(cl_uint);
-	cl_uint element4 = inputByte / sizeof(cl_uint4);
-	cl_uint element4PerThread = element4 / nThreads;
+	cl_context context;
 
-	unsigned int *input = (unsigned int*)malloc(inputByte);
-	unsigned int *cpuhist = (unsigned int*)malloc(outputNBytes);
-	unsigned int *gpuhist = (unsigned int*)malloc(outputNBytes);
-	memset(input, 0, inputByte);
-	memset(cpuhist, 0, outputNBytes);
-	memset(gpuhist, 0, outputNBytes);
+	context = clCreateContext(NULL, 1, device, NULL, NULL, &status);
 
-	int i;
-	time_t ltime;
-	cl_uint a, b;
-	time(&ltime);
-	for (i = 0, a = b = (cl_uint)ltime; i < element; i++)// b ÁöÑ‰Ωé 16 ‰Ωç‰πò a ÂÜçÂä†‰∏ä b ÁöÑÈ´ò 16 ‰ΩçÔºåËµãÁªôÊñ∞ÁöÑ b
-		input[i] = (b = (a * (b & 65535)) + (b >> 16));
+	check(status);
 
-	cl_mem inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputByte, input, &status);
-	cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, outputNBytes, gpuhist, &status);
+	cl_command_queue cmdQueue = clCreateCommandQueue(context, device[0], 0, &status);
 
-	clSetKernelArg(histogram, 0, sizeof(cl_mem), (void *)&inputBuffer);
-	clSetKernelArg(histogram, 1, sizeof(cl_mem), (void *)&outputBuffer);
-	clSetKernelArg(histogram, 2, sizeof(cl_uint), (void *)&element4PerThread);
-	clSetKernelArg(reduce, 0, sizeof(cl_mem), (void *)&outputBuffer);
-	clSetKernelArg(reduce, 1, sizeof(cl_uint), (void *)&nGroups);
+	check(status);
 
-	size_t globalSizeHist = nThreads, localSizeHist = nThreadsPerGroup;
-	size_t globalSizeReduce = SIZE_OF_BIN, localSizeReduce = 64;
+	cl_mem  bufInputImage = clCreateBuffer(context, CL_MEM_READ_ONLY, imageSize, NULL, &status);
 
-	clEnqueueNDRangeKernel(queue, histogram, 1, NULL, &globalSizeHist, &localSizeHist, 0, NULL, NULL);
-	clEnqueueNDRangeKernel(queue, reduce, 1, NULL, &globalSizeReduce, &localSizeReduce, 0, NULL, NULL);
-	clEnqueueReadBuffer(queue, outputBuffer, CL_TRUE, 0, outputNBytes / nGroups, gpuhist, 0, NULL, NULL);
+	check(status);
 
-	for (i = 0, input; i < element; i++)  // ‰ΩøÁî® CPU ËÆ°ÁÆóÔºåÊ≥®ÊÑèÊØè 8 ‰ΩçÁúãÂÅö‰∏Ä‰∏™ÂÖÉÁ¥†ÔºåÂèñÂÄºËåÉÂõ¥ 0 ~ 255
+	cl_mem bufOutputHistogramR = clCreateBuffer(context, CL_MEM_WRITE_ONLY, histogramSize, NULL, &status);
+
+	cl_mem bufOutputHistogramG = clCreateBuffer(context, CL_MEM_WRITE_ONLY, histogramSize, NULL, &status);
+
+	cl_mem bufOutputHistogramB = clCreateBuffer(context, CL_MEM_WRITE_ONLY, histogramSize, NULL, &status);
+
+	check(status);
+
+	status = clEnqueueWriteBuffer(cmdQueue, bufInputImage, CL_TRUE, 0, imageSize, img.data, 0, NULL, NULL);
+
+	check(status);
+
+	int zero = 0;
+
+	status = clEnqueueFillBuffer(cmdQueue, bufOutputHistogramR, &zero, sizeof(int), 0, histogramSize, 0, NULL, NULL);
+
+	status = clEnqueueFillBuffer(cmdQueue, bufOutputHistogramG, &zero, sizeof(int), 0, histogramSize, 0, NULL, NULL);
+
+	status = clEnqueueFillBuffer(cmdQueue, bufOutputHistogramB, &zero, sizeof(int), 0, histogramSize, 0, NULL, NULL);
+
+	//check(status);
+
+	/* Create a program with source code */
+	// Ω´clŒƒº˛÷–µƒ¥˙¬Î◊™Œ™◊÷∑˚¥Æ
+	string strSource;
+	
+	const char *pSource;
+	
+	status = ConvertToString("kernel.cl", strSource);
+	
+	pSource = strSource.c_str();			// ªÒµ√strSource÷∏’Î
+
+	size_t uiArrSourceSize = strlen(pSource);	// ◊÷∑˚¥Æ¥Û–°
+
+	// ¥¥Ω®≥Ã–Ú∂‘œÛ
+
+	cl_program program = clCreateProgramWithSource(context, 1, &pSource, &uiArrSourceSize, NULL);
+	
+	if (NULL == program)
 	{
-		cpuhist[(input[i] >> 24) & 0xff]++;
-		cpuhist[(input[i] >> 16) & 0xff]++;
-		cpuhist[(input[i] >> 8) & 0xff]++;
-		cpuhist[(input[i] >> 0) & 0xff]++;
+		cout << "Error: Can not create program" << endl;
+
+		return 0;
 	}
 
-	int countError;
-	for (i = countError = 0; i < SIZE_OF_BIN; i++)// Ê£ÄÊü•ÁªìÊûú
-	{
-		if (gpuhist[i] != cpuhist[i])
-			countError++;
-	}
-	printf("\n<main> CPU, GPU %s.\n\n\n", countError ? "mismatched" : "matched");
+	// -----------------------------6. ±‡“Î≥Ã–Ú--------------------------------
 
-	free(listPlatform);
-	free(listDevice);
-	free(input);
-	free(cpuhist);
-	free(gpuhist);
-	clReleaseContext(context);
-	clReleaseCommandQueue(queue);
+	// ±‡“Î≥Ã–Ú
+
+	status = clBuildProgram(program, 1, device, NULL, NULL, NULL);
+
+	if (CL_SUCCESS != status)	// ±‡“Î¥ÌŒÛ
+	{	cout << "Error: Can not build program" << endl;
+
+		char szBuildLog[16384];
+
+		clGetProgramBuildInfo(program, *device, CL_PROGRAM_BUILD_LOG, sizeof(szBuildLog), szBuildLog, NULL);
+
+		cout << "Error in Kernel: " << endl << szBuildLog;
+
+		clReleaseProgram(program);
+		
+		return 0;
+	}
+
+	cl_kernel kernel;
+
+	kernel = clCreateKernel(program, "histogramforRGB", &status);
+
+	check(status);
+
+	status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufInputImage);
+
+	status |= clSetKernelArg(kernel, 1, sizeof(int), &imageElements);
+
+	status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufOutputHistogramR);
+
+	status |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &bufOutputHistogramG);
+
+	status |= clSetKernelArg(kernel, 4, sizeof(cl_mem), &bufOutputHistogramB);
+
+	check(status);
+
+	size_t globalWorkSize[1];
+
+	globalWorkSize[0] = 1024;
+
+	size_t localWorkSize[1];
+
+	localWorkSize[0] = 64;
+
+	status = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+
+	check(status);
+
+	status = clEnqueueReadBuffer(cmdQueue, bufOutputHistogramR, CL_TRUE, 0, histogramSize, hOutputHistogramR, 0, NULL, NULL);
+
+	status = clEnqueueReadBuffer(cmdQueue, bufOutputHistogramG, CL_TRUE, 0, histogramSize, hOutputHistogramG, 0, NULL, NULL);
+
+	status = clEnqueueReadBuffer(cmdQueue, bufOutputHistogramB, CL_TRUE, 0, histogramSize, hOutputHistogramB, 0, NULL, NULL);
+
+	check(status);
+
+	for (int i = 0; i < HIST_BINS; i++) 
+	{
+
+		printf("histogram %d  OpenCL-result:R:%d G:%d B:%d---CPU-result:R:%d G:%d B:%d\n", i, hOutputHistogramR[i], hOutputHistogramG[i], hOutputHistogramB[i], rgbhistogram[i * 3], rgbhistogram[i * 3 + 1], rgbhistogram[i * 3 + 2]);
+
+	}
+
+	clReleaseKernel(kernel);
+
 	clReleaseProgram(program);
-	clReleaseKernel(histogram);
-	clReleaseKernel(reduce);
-	clReleaseMemObject(inputBuffer);
-	clReleaseMemObject(outputBuffer);
-	getchar();
+
+	clReleaseCommandQueue(cmdQueue);
+
+	clReleaseMemObject(bufInputImage);
+
+	clReleaseMemObject(bufOutputHistogramR);
+
+	clReleaseMemObject(bufOutputHistogramG);
+
+	clReleaseMemObject(bufOutputHistogramB);
+
+	clReleaseContext(context);
+
+	free(hOutputHistogramR);
+
+	free(hOutputHistogramG);
+
+	free(hOutputHistogramB);
+
+	//free(programSource);
 	return 0;
 }
